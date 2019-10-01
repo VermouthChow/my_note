ab5b316048753ca146e6b477f39a33dee5025a80
elle <zqchow@163.com>
2019-10-01 16:53:40 +0800

    init

---
+++ nt.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+class MyNote
+    DIR_PATH = "#{`echo $HOME`.chomp}/MyNote".freeze
+    CONTENT_PATH = "#{DIR_PATH}/content".freeze
+    VALID_ARGV_1 = %w{ l vn ve n e r rename del help }.freeze
+
+    Dir.mkdir(DIR_PATH) unless Dir.exist?(DIR_PATH)
+    Dir.mkdir(CONTENT_PATH) unless Dir.exist?(CONTENT_PATH)
+
+    def call
+        VALID_ARGV_1.include?(ARGV.first) ? send(ARGV.first) : error_handler('no such command')
+    end
+
+    private
+
+    def help
+        note = <<-EOF
+
+        first, alias nt=~/MyNote/nt.sh
+        chmod -u+x ~/MyNote/nt.sh if Permission denied
+
+        params:
+        
+        l                 - list the note
+        n file_name       - edit a note OR create a note
+        e file_name       - edit a note OR create a note
+        vn file_name      - edit a note OR create a note by vim
+        ve file_name      - edit a note OR create a note by vim
+        r file_name       - read a note
+        rename old new    - rename a note
+        del               - delete a note
+        EOF
+
+        puts note
+    end
+
+    def files
+        FileUtils.cd(CONTENT_PATH) do
+            fs = `ls -l | grep -v ^d|awk '{print $9}'`
+            puts fs.gsub("\n", "    ")
+        end
+    end
+
+    def edit_file
+        print ' > '
+        buf = STDIN.gets
+        File.open(full_path, 'a') { |f| f.puts buf }
+        read_file
+    end
+
+    def vim_edit_file
+        system "vim #{full_path}"
+        read_file
+    end
+
+    def read_file(file_full_name: full_path)
+        puts "----------------------- #{ARGV[1]} -----------------------\n\n"
+        File.open(file_full_name) do |f|
+            puts f.readlines
+        end
+    end
+
+    def rename
+        FileUtils.cd(CONTENT_PATH) do
+            File.rename(ARGV[1], ARGV[2])
+        end
+        puts "changed #{ARGV[1]} to - #{ARGV[2]}"
+    end
+
+    def del
+        FileUtils.rm(full_path)
+        puts "deleted #{ARGV[1]} success!"
+    end
+
+    def full_path
+        "#{CONTENT_PATH}/#{ARGV[1]}"
+    end
+
+    def error_handler(str)
+        puts str
+    end
+
+    alias_method :n, :edit_file
+    alias_method :e, :edit_file
+    alias_method :vn, :vim_edit_file
+    alias_method :ve, :vim_edit_file
+    alias_method :l, :files
+    alias_method :r, :read_file
+end
+
+MyNote.new.call
+
